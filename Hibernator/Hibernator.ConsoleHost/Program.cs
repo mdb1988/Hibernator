6f95f5cb089aa8316dd2fc2efcb2069ea1f5e043
triffids <ma.debisschop@gmail.com>
2015-11-23 20:39:49 +0100

    fixed bad formatting

--- Hibernator/Hibernator.ConsoleHost/Program.cs
+++ Hibernator/Hibernator.ConsoleHost/Program.cs
@@ -8,60 +8,60 @@ using System.Threading.Tasks;
 
 namespace Hibernator.ConsoleHost
 {
-	public class Program
-	{  
-		   static Watcher w = new Watcher();
-					static Thread listenThread = new Thread(Listener);
-					static Thread watcherThread = new Thread(Watch);
-					static ManualResetEvent _suspendEvent = new ManualResetEvent(true);
+    public class Program
+    {
+        static Watcher w = new Watcher();
+        static Thread listenThread = new Thread(Listener);
+        static Thread watcherThread = new Thread(Watch);
+        static ManualResetEvent _suspendEvent = new ManualResetEvent(true);
 
-				public static void Main(string[] args)
-				{					
+        public static void Main(string[] args)
+        {
 
-				var started = DateTime.Now;
-    Console.WriteLine("Started hibernate watch at " + started);
-	    listenThread = new Thread(Listener);
-					listenThread.Start();
-				 watcherThread = new Thread(Watch);
-					watcherThread.Start();				
-				}
-									
+            var started = DateTime.Now;
+            Console.WriteLine("Started hibernate watch at " + started);
+            listenThread = new Thread(Listener);
+            listenThread.Start();
+            watcherThread = new Thread(Watch);
+            watcherThread.Start();
+        }
 
-				public static void Watch()
-				{
-					while(true)
-					{					
-				_suspendEvent.WaitOne(Timeout.Infinite);
-					w.Watch();				
-					}
-					
-				}
-				
-				public static void Listener()
-				{				
-					while (true)
-					{
-								
-							if ((!(Console.KeyAvailable && Console.ReadLine() == "change")))
-							{
-							
-							}
-							else
-							{				
-							_suspendEvent.Reset();									
-							Do();										
-							_suspendEvent.Set();											
-							}
-					}				
-		 }
-			
-			public static void Do()
-			{					
-					 	Console.WriteLine("Set new timeout (in minutes)");
-							var newTimeout = Console.ReadLine();
-							var newT = Convert.ToInt16(newTimeout);
-							w.UpdateParams(newT);
-			}				
 
-	}
+        public static void Watch()
+        {
+            while (true)
+            {
+                _suspendEvent.WaitOne(Timeout.Infinite);
+                w.Watch();
+            }
+
+        }
+
+        public static void Listener()
+        {
+            while (true)
+            {
+
+                if ((!(Console.KeyAvailable && Console.ReadLine() == "change")))
+                {
+
+                }
+                else
+                {
+                    _suspendEvent.Reset();
+                    Do();
+                    _suspendEvent.Set();
+                }
+            }
+        }
+
+        public static void Do()
+        {
+            Console.WriteLine("Set new timeout (in minutes)");
+            var newTimeout = Console.ReadLine();
+            var newT = Convert.ToInt16(newTimeout);
+            w.UpdateParams(newT);
+        }
+
+    }
 }
